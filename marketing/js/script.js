9c528722be29546f0e576f78d915204c09d087c8
johnebah <70802507+johnebah@users.noreply.github.com>
2021-07-26 23:44:54 +0100

    Add files via upload

---
+++ marketing/js/script.js
@@ -0,0 +1,11 @@
+function toggle() {
+	document.getElementById('menu').classList.toggle('display');
+}
+
+function dtoggle(){
+	document.getElementById('list').style.left="0%";
+}
+function savage(){
+	document.getElementById('list').style.left="-400%";
+}
+
